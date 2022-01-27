9991ea6c9ef465f4d04f214b3b93aac9a0b17345
Khandice <s.khandice@gmail.com>
2022-01-26 19:49:47 -0800

    Initial Commit

---
+++ NewLeaf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewLeaf
+//
+//  Created by Khandice Schuhmann on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewLeaf/NewLeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLeafApp.swift
+//  NewLeaf
+//
+//  Created by Khandice Schuhmann on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct NewLeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
