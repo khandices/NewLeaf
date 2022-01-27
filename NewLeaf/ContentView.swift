b33b159e8f5dd1eb686e2de1ab324c8ae394c2f4
Khandice <s.khandice@gmail.com>
2022-01-27 09:00:12 -0800

    Creates log in and create user view page along with adding firebase to the project

--- NewLeaf/ContentView.swift
+++ NewLeaf/ContentView.swift
@@ -6,16 +6,122 @@
 //
 
 import SwiftUI
+import Firebase
 
+
+/* To read or write data from the database, you need an instance of FIRDatabaseReference:
+
+ var ref: DatabaseReference!
+
+ ref = Database.database().reference()
+ */
 struct ContentView: View {
+
+        // CREATE //
+        /*  Sets the value in the db.
+    self.ref.child("users").child(user.uid).setValue(["username": username])
+        */
+        /* Using setValue in this way overwrites data at the specified location, including any child nodes. However, you can still update a child without rewriting the entire object. If you want to allow users to update their profiles you could update the username as follows:
+         self.ref.child("users/\(user.uid)/username").setValue(username)
+         */
+
+        // READ //
+        /*
+         You can use the FIRDataEventTypeValue event to read the data at a given path, as it exists at the time of the event. This method is triggered once when the listener is attached and again every time the data, including any children, changes. The event callback is passed a snapshot containing all data at that location, including child data. If there is no data, the snapshot will return false when you call exists() and nil when you read its value property.
+
+         refHandle = postRef.observe(DataEventType.value, with: { snapshot in
+          // ...
+        })
+        */
+
+    @State private var username = ""
+    @State private var userEmail = ""
+    @State private var userPassword = ""
+    @State private var userLocation = ""
+    @State var isLoginMode = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                VStack (spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    } .pickerStyle(SegmentedPickerStyle())
+                        .padding()
+
+                    if !isLoginMode {
+                        Button {
+
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                        }
+                        Section {
+                            TextField("Username", text: $username )
+                                .padding(12)
+                                .background(Color.white)
+                        }
+
+                        Section {
+                            TextField("Location", text: $userLocation)
+                                .padding(12)
+                                .background(Color.white)
+                        }
+                    }
+
+                    Section {
+                        TextField("Email", text: $userEmail)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .padding(12)
+                            .background(Color.white)
+                    }
+
+                    Section {
+                        SecureField("Password", text: $userPassword)
+                            .padding(12)
+                            .background(Color.white)
+                    }
+
+
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account!")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        } .background(Color.green)
+                    }
+                }
+                .padding()
+                .background(Color(.init(white: 0, alpha: 0.05)))
+                        .ignoresSafeArea()
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+        }
+
     }
-}
+
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing creds")
+        } else {
+            print("Registers a new account inside Firebase Auth and then stores image in storage somehow")
+        }
+    }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+}
+
--- NewLeaf/NewLeafApp.swift
+++ NewLeaf/NewLeafApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 @main
 struct NewLeafApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+    init() {
+         FirebaseApp.configure()
+     }
+     
+     var body: some Scene {
+         WindowGroup {
+             ContentView()
+//             UserProfileView()
+         }
+     }
+ }
+
